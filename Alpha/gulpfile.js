5eb61a4de2e817740f5062ac05e890a112bbcaad
zhuangtongfa <zhuangtongfa@qq.com>
2016-04-03 11:24:25 +0800

    improve,add js change reload

--- Alpha/gulpfile.js
+++ Alpha/gulpfile.js
@@ -28,7 +28,8 @@ var formatDateTime = function() {
           server: "./"+now
       });
       gulp.watch("./"+now+"/src/sass/*.scss", ['scssToCss']);
-      gulp.watch("./"+now+"/*.html").on('change', reload);
+      gulp.watch("./"+now+"/src/*/*.js").on('change', reload);
+      gulp.watch(["./"+now+"/*.html","./"+now+"/*/*.html","./"+now+"/*/*/*.html"]).on('change', reload);
   });
 
   console.log(now)
